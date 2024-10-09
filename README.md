e952b491cd2cff57e3364a2d463129cc17bbd25d
Peng Chengling <76205464+3522875020@users.noreply.github.com>
2024-10-09 22:04:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,32 +1,28 @@
 # WeChatPush
 
-基于itchat的微信消息接收端 感谢itchat大佬 和itchat-uos开发 因为我有些地方需要更改就单独下载了itchat的包然后更改的
+基于itchat的微信消息接收端，感谢itchat大佬和itchat-uos开发。因为我有些地方需要更改就单独下载了itchat的包然后更改的。
 
-你可以挂在服务器上或者 电脑上 亦或者 单片机上 
+你可以挂在服务器上或者电脑上，亦或者单片机上。
 
 [itchat](https://github.com/littlecodersh/ItChat)
 
 [itchat-uos](https://github.com/why2lyj/ItChat-UOS)
 
-包自己导吧 环境文件我就不删了 嘻嘻
+包自己导吧，环境文件我就不删了，嘻嘻。
 
-⚠ 需要修改 itchat/config 内的 设备ID 和phone 0对应小米 1对应oppo 2对应华为 4对应腾讯推送 3对应fcm （服务器还没整）5bark
+⚠ 需要修改 itchat/config 内的 DINGTALK_WEBHOOK 为你的钉钉机器人 webhook 地址。
 
-不接收的消息来自用户名 写在itchat/config BLOCK_NAME 数组里 包含关系 只需要输入前几位就行
+不接收的消息来自用户名写在 itchat/config BLOCK_NAME 数组里，包含关系，只需要输入前几位就行。
 
-目前mipush 腾讯云推送 支持直接回复 腾讯云需要在itchat/config 里的 MES_THROUGH 改为 1 
+目前支持钉钉推送，快速回复需要在 FarPush 快速回复里填写你的服务器地址，像这样 http://192.168.0.1:9091/send。
 
-快速回复需要在FarPush 快速回复里填写你的服务器地址 像这样 http://192.168.0.1:9091/send
-这样的话FarPush 在接收到消息 会发送支持回复的通知 然后通知你的服务器 发送消息 用的是python flask 端口默认在9091 可能需要您开启防火墙 或者自行更改端口
+快速查看语音或者图片请在快速回复里填入你的服务器地址 http://192.168.0.1:9091/send，这样后面的/send 不影响，会自动处理。
 
-快速查看语音 或者图片 请在快速回复里填入你的服务器地址 http://192.168.0.1:9091/send 这样 后面的/send 不影响 会自动处理
-存储的文件在 files 文件夹内 FarPush 存储在 /storage/emulated/0/android/data/com.farplace.qingzhuo/files/media 内
-目前没写自动清理 后面可能有 或者有大佬动手写下把 呜呜呜
+存储的文件在 files 文件夹内。
 
+如果 window 等需要使用图片请在 main.py 里 itchat 参数内删掉 enablecmdqr。
 
-如果window 等需要使用图片请在main.py 里 itchat 参数内删掉 enablecmdqr
-
-后台运行 请使用 nohup python3 main.py& tail -f nohup.out
+后台运行请使用 nohup python3 main.py& tail -f nohup.out
 
 [FarPush](www.coolapk.com/apk/com.farplace.farpush)
 
@@ -38,9 +34,9 @@
 
 CentOS 还需要 yum install xdg-utils
 
-# 欢迎star 嘻嘻嘻 感谢您的帮助
+# 欢迎 star 嘻嘻嘻 感谢您的帮助
 
-我不会python 所以就只在itchat基础上加了推送的代码
+我不会 python 所以就只在 itchat 基础上加了推送的代码
 
 # 2023/2/6 
 
@@ -49,13 +45,17 @@ CentOS 还需要 yum install xdg-utils
 # 2024/1/10
 
 没钱续费服务器 
-⚠ 我公开了服务器端的jar 包以后大家就跑这个jar 包到你的服务器吧 然后把python 里的推送地址改成你的服务器ip
+⚠ 我公开了服务器端的 jar 包以后大家就跑这个 jar 包到你的服务器吧 然后把 python 里的推送地址改成你的服务器 ip
 
 再见
 
 # 2024/2/28
 
-小米商店上架了可分享内容的app 咫尺妙享 或许很快又会再见😂
+小米商店上架了可分享内容的 app 咫尺妙享 或许很快又会再见😂
 # 2024/9/21
 
 新的服务器买好了一年期限 可以继续用推送了哈哈😄 
+
+## 环境变量设置
+
+在运行程序之前，请确保设置以下环境变量：
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import logging
 import requests
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, render_template
 import itchat
 import itchat.content
-import itchat.config
+from itchat.config import (DINGTALK_APP_KEY, DINGTALK_APP_SECRET, 
+                           LOG_LEVEL)
 import farpush
 import socket
 import json
@@ -10,11 +12,28 @@ import _thread
 import os
 import datetime
 
+# Configure logging
+logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
 
 # store messages
 messages = []
 
+def get_access_token():
+    url = "https://api.dingtalk.com/v1.0/oauth2/accessToken"
+    headers = {"Content-Type": "application/json"}
+    data = {"appKey": DINGTALK_APP_KEY, "appSecret": DINGTALK_APP_SECRET}
+    logger.debug(f"Sending request to {url} with data: {json.dumps(data)}")
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        response_data = response.json()
+        logger.info("Successfully obtained access token")
+        return response_data.get("accessToken")
+    else:
+        logger.error(f"Failed to get access token: {response.status_code} {response.text}")
+        return None
 
 @app.route("/send", methods=['POST'])
 def received():
@@ -22,58 +41,58 @@ def received():
     username = data['username']
     nametype = data['type']
     content = data['content']
+    logger.info(f"Received message from {username}: {content}")
     if nametype == '0':
         send4nick(username, content)
     else:
         send(username, content)
     return 'ok'
 
-
 @app.route("/getuser", methods=['POST'])
 def getuser():
     data = request.json
     username = data['username']
+    logger.info(f"Fetching user info for {username}")
     friends = itchat.search_friends(name=username)
     if friends:
         author = friends[0]
         user = {'nickName': author.nickName, 'remarkName': author.remarkName, 'headImage': author.get_head_image_url()}
     return json.dumps(user)
 
-
 @app.route("/getuserphoto", methods=['POST'])
 def getuserphoto():
     data = request.json
     username = data['username']
+    logger.info(f"Fetching user photo for {username}")
     friends = itchat.search_friends(name=username)
     if friends:
         author = friends[0]
         return itchat.get_head_img(author.userName)
     return "fail"
 
-
 # 调用定义的方法
 @app.route("/getmessagelist", methods=['GET'])
 def getmessage():
+    logger.info("Fetching message list")
     message = {"messages": messages}
     return json.dumps(message)
-
 
 @app.route("/getfile", methods=['POST'])
 def getfile():
     filename = request.json['filename']
+    logger.info(f"Sending file: {filename}")
     return send_file(filename)
-
 
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
     message = {}
-    print((msg.user.remarkName or msg.user.nickName) + " 说 : " + msg.text)
-    message['name'] = (msg.user.remarkName or msg.user.nickName)
+    sender = msg.user.remarkName or msg.user.nickName
+    logger.info(f"Received text message from {sender}: {msg.text}")
+    message['name'] = sender
     message['content'] = msg.text
     message['time'] = int(datetime.datetime.now().timestamp())
     messages.append(message)
-    farpush.mespush((msg.user.remarkName or msg.user.nickName), msg.text)
-
+    farpush_instance.push_text(sender, msg.text)
 
 @itchat.msg_register(itchat.content.MEDIA_TYPE_MSG)
 def text_media(msg):
@@ -81,42 +100,44 @@ def text_media(msg):
         msgtext = itchat.content.MESSAGE_TEXT[msg.type]
     else:
         msgtext = '未定义类型'
-    print((msg.user.remarkName or msg.user.nickName) + " 发送了 : " + msgtext)
-    farpush.mespush((msg.user.remarkName or msg.user.nickName), msgtext)
-
+    sender = msg.user.remarkName or msg.user.nickName
+    logger.info(f"Received media message from {sender}: {msgtext}")
+    farpush_instance.push_text(sender, msgtext)
 
 @itchat.msg_register([itchat.content.RECORDING, itchat.content.PICTURE])
 def mes_media(msg):
     if not os.path.exists("files"):
         os.mkdir("files")
     msg.download(msg.fileName)
-    farpush.mdpush((msg.user.remarkName or msg.user.nickName), itchat.content.MESSAGE_TEXT[msg.type], msg.fileName)
-
+    sender = msg.user.remarkName or msg.user.nickName
+    logger.info(f"Downloaded media file: {msg.fileName}")
+    farpush_instance.push_text(sender, f"Received media: {msg.fileName}")
 
 # for group
 @itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
 def text_reply(msg):
-    farpush.mespush(msg.user.nickName, msg.text)
-
+    logger.info(f"Received group text message from {msg.user.nickName}: {msg.text}")
+    farpush_instance.push_text(msg.user.nickName, msg.text)
 
 def send(username, content):
+    logger.info(f"Sending message to {username}: {content}")
     itchat.send(content, toUserName=username)
 
-
 def send4nick(nickname, content):
+    logger.info(f"Sending message to nickname {nickname}: {content}")
     friends = itchat.search_friends(name=nickname)
     if friends:
         author = friends[0]
         author.send(content)
 
-
 def flask(ip, port):
     from waitress import serve
+    logger.info(f"Starting Flask server on {ip}:{port}")
     serve(app, host=ip, port=port)
 
-
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=9091)
+    logger.info("Starting application")
+    farpush_instance = farpush.farpush()
     _thread.start_new_thread(flask, ('0.0.0.0', 9091))
     itchat.check_login()
     itchat.auto_login(hotReload=True, enableCmdQR=2)
